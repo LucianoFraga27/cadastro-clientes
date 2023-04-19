88a6db3539c8579a1b9fd76db5e9bfe650f47a07
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-04-19 12:07:22 -0300

    Commit Inicial

---
+++ backend/src/main/java/com/aps/backend/BackendAps1Application.java
@@ -0,0 +1,13 @@
+package com.aps.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAps1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAps1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aps/backend/BackendAps1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAps1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
